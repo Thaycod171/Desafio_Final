65e233198ac5109065aa9f67f7f62d6c85fceaed
Thaycod171 <nunesthayane057@gmail>
2025-05-21 20:19:59 -0300

    CI/CD e adicionando coisas ao dockerfile

--- lab_api/test_app.py
+++ lab_api/test_app.py
@@ -12,16 +12,22 @@ class APITestCase(unittest.TestCase):
         # Cria um cliente de teste da API
         cls.client = app.test_client()
 
+
+#Verifica se a rota / responde com o status 200 e a mensagem correta
     def test_home(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {"message": "API is running"})
 
+
+#Confirma que a rota /login gera um token JWT
     def test_login(self):
         response = self.client.post('/login')
         self.assertEqual(response.status_code, 200)
         self.assertIn('access_token', response.json)
 
+
+#Testa o acesso à rota /protected sem o token JWT, esperando um status de não autorizado (401). 
     def test_protected_no_token(self):
         response = self.client.get('/protected')
         self.assertEqual(response.status_code, 401)
